177feadc6d1a8e8303b2667055351acc636ceb1b
bibhuprasadrout <bibhu.rs@gmail.com>
2023-03-28 01:26:05 +0530

    Add content to connect

--- src/View/Contact/Contact.js
+++ src/View/Contact/Contact.js
@@ -1,7 +1,24 @@
+import styled from "styled-components";
+import { Card } from "../../DesignSystem/Utils/Card/Card";
+const ContactCard = styled(Card)`
+  color: ${(props) => props.theme.light};
+  .email {
+    font-size: 1.3rem;
+    color: ${(props) => props.theme.dark};
+    display: block;
+    max-width: fit-content;
+    margin: 0 auto;
+  }
+`;
 export const Contact = () => {
   return (
     <>
-      <div>Contact</div>
+      <ContactCard>
+        <div>Lets get to know eachother. Drop me a mail at...</div>
+        <a href="mailto:bibhu.rs@gmail.com" className="email">
+          bibhu.rs@agmail.com
+        </a>
+      </ContactCard>
     </>
   );
 };
